04f29d7eecb7b21de2ef120bfe73d4b2e40d8cdb
DEWAN <dewanmahinchy@gmail.com>
2022-03-28 00:02:20 +0600

    update quantity function done at case cart

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-function updateProductQuantity(isIncreasing){
-    const productValue = document.getElementById('phone-quantity');
-    const productQuantity = parseInt(productValue.value);
+function updateProductQuantity(product, price, isIncreasing){
+    const productValue = document.getElementById(product + '-quantity');
+    let productQuantity = parseInt(productValue.value);
+    // Increase or decrease product quantity
     if(isIncreasing){
-        productValue.value = productQuantity + 1;
-    }else{
-        productValue.value = productQuantity - 1;
+        productQuantity = productQuantity + 1;
+    }else if(productQuantity > 0){
+        productQuantity = productQuantity - 1;
     }
+    productValue.value = productQuantity;
+    // Update the price amount based on product quantity
+    const productPrice = document.getElementById(product + '-price');
+    productPrice.innerText = price * productQuantity;
 }
 
 //! Phone cart
 document.getElementById('phone-plus').addEventListener('click', function(){
-    updateProductQuantity(true)
+    updateProductQuantity("phone", 1219, true)
 })
 document.getElementById('phone-minus').addEventListener('click', function(){
-    updateProductQuantity(false)
+    updateProductQuantity("phone", 1219, false)
+})
+
+//! Case cart
+document.getElementById('case-plus').addEventListener('click', function(){
+    updateProductQuantity("case", 59, true)
+})
+document.getElementById('case-minus').addEventListener('click', function(){
+    updateProductQuantity("case", 59, false)
 })
